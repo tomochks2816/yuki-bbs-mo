139bab085feb3323c6f346789ab0b89d383f9d22
mersnn621 <mersnn621@gmail.com>
2023-08-23 18:23:18 +0900

    Update main.py

--- main.py
+++ main.py
@@ -19,8 +19,8 @@ version = "1.0"
 
 def get_info(request):
     global version
-    return json.dumps(["1.0", "https://yuki-tangolevel-o341.onrender.com", "[(b'host', b'bbs.mersnn621.com'), (b'user-agent', b'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'), (b'accept', b'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7'), (b'accept-encoding', b'gzip'), (b'accept-language', b'ja,en-US;q=0.9,en;q=0.8'), (b'cdn-loop', b'cloudflare; loops=1; subreqs=1'), (b'cf-connecting-ip', b'2403:7800:b72b:1d00:9533:2921:2f08:1683'), (b'cf-connecting-o2o', b'1'), (b'cf-ew-via', b'15'), (b'cf-ipcountry', b'JP'), (b'cf-ray', b'7fa15e3c646baf2e-NRT'), (b'cf-visitor', b'{\"scheme\":\"https\"}'), (b'cf-worker', b'onrender.com'), (b'cookie', b'cf_clearance=d4YY4f5pw_MK_RAANT77IbPvGKRDHLoAkSHGqNZuJhU-1686225421-0-160; googtransopt=os=1'), (b'priority', b'u=0, i'), (b'referer', b'https://bbs.mersnn621.com/bbs'), (b'render-proxy-ttl', b'4'), (b'sec-ch-ua', b'\"Not.A/Brand\";v=\"8\", \"Chromium\";v=\"114\", \"Google Chrome\";v=\"114\"'), (b'sec-ch-ua-mobile', b'?0'), (b'sec-ch-ua-platform', b'\"macOS\"'), (b'sec-fetch-dest', b'document'), (b'sec-fetch-mode', b'navigate'), (b'sec-fetch-site', b'same-origin'), (b'sec-fetch-user', b'?1'), (b'true-client-ip', b'2403:7800:b72b:1d00:9533:2921:2f08:1683'), (b'upgrade-insecure-requests', b'1'), (b'x-forwarded-for', b'2403:7800:b72b:1d00:9533:2921:2f08:1683,2403:7800:b72b:1d00:9533:2921:2f08:1683, 172.71.150.206, 10.214.165.150'), (b'x-forwarded-proto', b'https'), (b'x-request-start', b'1692605096501843')]", "7fd58e81589"])
-    #return json.dumps([version,os.environ.get('RENDER_EXTERNAL_URL'),str(request.scope["headers"]),str(request.scope['router'])[39:-2]])
+    #return json.dumps()
+    return json.dumps([version,os.environ.get('RENDER_EXTERNAL_URL'),str(request.scope["headers"]),str(request.scope['router'])[39:-2]])
 
 
 
